21fd75b46199584dcf261bbb88ef4b97c1755515
haleema28 <sadiyahaleema2004@gmail.com>
2025-07-13 21:08:08 +0530

    Implement auth, appwrite, and sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,35 +1,36 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox';
-import React from 'react'
-import RightSidebar from '@/components/RightSidebar';
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
+import RightSidebar from "@/components/RightSidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = () => {
-  const loggedIn = { firstName: 'Haleema',lastName: 'Sadiya',email:'sadiyahaleema2004@gmail.com'};
+const Home = async() => {
+  const loggedIn = await getLoggedInUser();
   return (
-    <section className='home'>
-      <div className='home-content'>
-        <header className='homeHeader'>
-        <HeaderBox
-         type='greeting'
-         title='welcome'
-         user={loggedIn?.firstName || 'Guest'}
-         subtext='Access and manage your account and transactions efficiently.'
-        />
-        <TotalBalanceBox 
+    <section className="home">
+      <div className="home-content">
+        <header className="homeHeader">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.name || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
           />
-          </header>
-          RECENT TRANSACTIONS
-        </div>
-        <RightSidebar
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
         user={loggedIn}
-        transactions ={[]}
-        banks={[{currentBalance: 123.50},{currentBalance: 500.50}]}
-        />
-        </section>
-  )
-}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
